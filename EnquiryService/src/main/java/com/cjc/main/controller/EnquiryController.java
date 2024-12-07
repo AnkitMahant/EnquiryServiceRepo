938afb4dfca243f393af11feaf889cb2799cebd6
AnkitMahant <ankitmahant786@gmail.com>
2024-12-07 20:55:21 +0530

    Post by Developer1

--- EnquiryService/src/main/java/com/cjc/main/controller/EnquiryController.java
+++ EnquiryService/src/main/java/com/cjc/main/controller/EnquiryController.java
@@ -1,10 +1,32 @@
 package com.cjc.main.controller;
 
+import java.net.http.HttpClient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class EnquiryController {
+import com.cjc.main.model.EnquiryDetails;
+import com.cjc.main.serviceI.EnquiryServiceI;
 
-	
-	
+@RequestMapping("/enquiry")
+@RestController
+
+public class EnquiryController 
+{
+	@Autowired private EnquiryServiceI enquiryService;
+		
+	@PostMapping("/save-enquiry")
+	public ResponseEntity<EnquiryDetails>onSaveEnquiry(@RequestBody EnquiryDetails enquiryDetails)
+	{
+
+		EnquiryDetails enquiryDetailsRef=enquiryService.saveEnquiry(enquiryDetails);
+		return new ResponseEntity<EnquiryDetails>(enquiryDetailsRef,HttpStatus.CREATED);
+		
 }
+}
+
--- EnquiryService/src/main/java/com/cjc/main/serviceImpl/EnquiryServiceImpl.java
+++ EnquiryService/src/main/java/com/cjc/main/serviceImpl/EnquiryServiceImpl.java
@@ -1,11 +1,23 @@
 package com.cjc.main.serviceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cjc.main.model.EnquiryDetails;
+import com.cjc.main.repository.EnquiryRepository;
 import com.cjc.main.serviceI.EnquiryServiceI;
 @Service
 public class EnquiryServiceImpl  implements EnquiryServiceI
 {
+
+	@Autowired private EnquiryRepository enquiryRepository;
+	
+	
+	@Override
+	public EnquiryDetails saveEnquiry(EnquiryDetails enquiryDetails) {
+		
+		return enquiryRepository.save(enquiryDetails);
+	}
 	
 	
 
